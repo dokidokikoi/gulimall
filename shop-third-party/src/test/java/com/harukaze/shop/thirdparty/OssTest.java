884a9697d347d543437a07efec32565a5ca379d1
dokidokikoi <60416355+dokidokikoi@users.noreply.github.com>
2022-04-21 20:09:53 +0800

    Update OssTest.java

--- shop-third-party/src/test/java/com/harukaze/shop/thirdparty/OssTest.java
+++ shop-third-party/src/test/java/com/harukaze/shop/thirdparty/OssTest.java
@@ -30,8 +30,8 @@ public class OssTest {
         //        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 //        String endpoint = "https://oss-cn-shenzhen.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId =  "LTAI5tLj1ueqGUKVYCsPo2Gv";
-//        String accessKeySecret = "2xYG9OACZXOWnprbluGZ3AVIbU0DTu";
+//        String accessKeyId =  "LTAgdfhdjjhhfdlujp2Gv";
+//        String accessKeySecret = "2xYG9OACZjrftyfkkrytesIbU0DTu";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "harukaze-gulimall";
         // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
